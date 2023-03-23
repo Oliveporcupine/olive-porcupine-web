3aab4e7fece2a6c2534cdc8e2e3331203e9cfb01
agrabau12 <annagrabau12@gmail.com>
2023-03-23 11:56:53 -0400

    commit navigation-2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -10,7 +9,9 @@ function App() {
     document.querySelector(".sidebar")?.classList.add("open");
   }
   return (
+   <Router>
     <div className="grid-container">
+        <link rel="stylesheet" href="style.css" />
 <head>
     <title> Olive Porcupine</title>
 </head>
@@ -22,8 +23,8 @@ function App() {
                 <a href="index.html">Olive Porcupine </a>
             </div>
             <div className="header-links">
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a> 
+                <Link to="/">Home</Link> 
+                <Link to="/catalog">Catalog</Link>
             </div>
 
         </header>
@@ -40,55 +41,11 @@ function App() {
                 </li>
             </ul>
         </aside>
-        <main>
-            <div className="content">
-                <ul className="products">
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="product"></img>
-                            <div className="product-name">
-                                <a href="product.html">Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="product"></img>
-                            <div className="product-name">
-                                <a href="product.html">Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                        </div>
-                    </li> 
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="product"></img>
-                            <div className="product-name">
-                                <a href="product.html">Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="product"></img>
-                            <div className="product-name">
-                                <a href="product.html">Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+        <main className= "main">
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/catalog" element={<Products />} />
+            </Routes>
         </main>
         <footer className= "footer">
             &copy; 2022 Olive Porcupine
@@ -96,7 +53,11 @@ function App() {
     </div>
 </body>
 </div>
+</Router>
   );
 }
 
 export default App;
+import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
